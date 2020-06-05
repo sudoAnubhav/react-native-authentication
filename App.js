180962c82cb327859e52ae90f3c8a38668132079
sudoAnubhav <60476751+sudoAnubhav@users.noreply.github.com>
2020-06-05 15:32:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Navigation from './Navigation';
+
+const App: () => React$Node = () => {
+    return (
+
+        <Navigation/>
+      
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
